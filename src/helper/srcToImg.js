97565cafe8e10f7ea1bcd3094bac089c4e603b11
blue <zhouxinzhe1@gmail.com>
2018-04-24 23:24:04 +0800

    爬虫测试

---
+++ src/helper/srcToImg.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const {promisify} = require('util');
+const writeFile = promisify(fs.writeFile);
+
+module.exports = async (src, dir) => {
+    if(/\.(jpg|png|gif)$/.test(src)) {
+        await urlToImg(src, dir);
+    }else {
+        await base64ToImg(src, dir);
+    }
+}
+
+// url => image
+const urlToImg = (url, dir, callback) => {
+    const mod = /^https:/.test(url) ? https : http;
+    const ext = path.extname(url);
+    const file = path.join(dir, `${Date.now()}${ext}`);
+
+    mod.get(url, res => {
+        res.pipe(fs.createWriteStream(file))
+            .on('finish', () => {
+                callback && callback();
+                console.log(file);
+            })
+    })
+}
+
+// base64 => image
+
+const base64ToImg = async(base64Str, dir) => {
+    // data:image/jpeg;/asdasd...
+    const matches = base64Str.match(/^data:(.+?);(.+)$/);
+    try {
+        const ext = matches[1].split("/")[1]
+        .replace('jpeg', 'jpg');
+        const file = path.join(dir, `${Date.now()}.${ext}`);
+        await writeFile(file, matches[2], 'base64');
+    } catch (ex) {
+        console.log("base64解析失败: ", ex);
+    }
+}
---
+++ src/mn.js
@@ -0,0 +1,60 @@
+const puppeteer = require("puppeteer");
+const {mn} = require("./config/default");
+const srcToImg = require("./helper/srcToImg");
+
+const screenSize = {
+    width: 1920,
+    height: 1080
+};
+const puppeteerDefaultOptions = {
+    headless: false
+    // devtools: false,
+    // args: [
+    //     `--window-size=${screenSize.width},${screenSize.height}`,
+    // ],
+};
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    
+    try {
+        await page.goto('https://image.baidu.com/');
+    } catch (error) {
+        console.log(error);
+        browser.close();
+    }
+    console.log('go to https://image.baidu.com/');
+    
+    
+    await page.setViewport(screenSize);
+    console.log('reset viewport');
+    
+    await page.focus("#kw");
+    await page.keyboard.sendCharacter("狗");
+    console.log('--before click--');
+    
+    await page.click('.s_search');
+    console.log('go to search list');
+
+    page.on("load", async () => {
+        console.log('page load done, start fetch...');
+        
+        const srcs = await page.evaluate(() => {
+            const images = document.querySelectorAll("img.main_img");
+            return Array.prototype.map.call(images, image => image.src);
+
+        })
+        console.log(`get ${srcs.length} images, start download`);
+
+        srcs.forEach(src => {
+            // sleep ：低频 避免 出发反爬虫逻辑
+            await page.waitFor(200);
+            srcToImg(src, mn);
+        });
+
+        await browser.close();
+    })
+    
+
+})().catch(err => console.error(err));
